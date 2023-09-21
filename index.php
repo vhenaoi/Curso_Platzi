c83dceab7643d00831796c868de3b48b2678634f
vhenaoi <36336800+vhenaoi@users.noreply.github.com>
2023-09-21 18:32:23 +0000

    calculadora

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+<?php 
 
-  <!--
-  This script places a badge on your repl's full-browser view back to your repl's cover
-  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-  teal, blue, blurple, magenta, pink!
-  -->
-  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
-  </body>
-</html>
+function suma(){
+      $valor1 = 8;
+      $valor2 = 6;
+      $resultado = $valor1 + $valor2;
+      print_r($resultado);
+    } 
+
+suma();
+?> 
