7909992bb399f8d0f4dc25484994a92a7aa8e114
vhenaoi <36336800+vhenaoi@users.noreply.github.com>
2023-09-21 17:46:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+///De python a JS
+//frase = 'las flores amarillas'
+//inicial = 4
+//final = 10
+//palabra = frase[inicial:final]
+//print(palabra)
+
+
+// Definición de variables
+let frase = 'las flores amarillas' 
+let inicial = 4 //variable local
+let final = 10 //variable local
+
+// Extraer palabra
+palabra = frase(inicial:final)
+
+
+//Mostrar en pantalla
+console.log(palabra)
---
+++ numeroMayor.js
@@ -0,0 +1,30 @@
+//El reto de esta clase es que encuentres el número más grande de una lista usando JavaScript.
+//El algoritmos/ diagrama de flujo es el siguiente:
+//Recibe un array de números (de cualquier cantidad).
+//Crea una variable “máxima” que inicie en 0.
+//Recorre el array valor por valor.
+//Pregunta si el valor de la variable máxima es mayor que el valor del número en la posición actual del array.
+//Si la condición anterior es verdadera, asigna el valor de la posición actual del array a la variable máxima.
+//Imprime el número más grande del array.
+  
+// Definición de variables
+let numeros = [5, 10, 15, 60, 8] //variable local
+let numeroMaximo = 0 //variable local
+let tamano = numeros.length // longitud de la variable
+
+
+// Ciclo
+for(let i=0; i<tamano; i++) {
+  if(numeroMaximo < numeros[i]){
+    numeroMaximo = numeros[i]
+  }
+}
+
+//Mostrar en pantalla
+console.log(numeroMaximo)
+
+//Uso de funciones de la libreria Math que encuentra el maximo en una lista
+let maximo = Math.max(5, 10, 15, 60, 8)
+
+//Mostrar en pantalla
+console.log(maximo)
