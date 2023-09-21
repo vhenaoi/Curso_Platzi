d554208f8a2de692522258c83ccbf0e26bdac572
vhenaoi <36336800+vhenaoi@users.noreply.github.com>
2023-09-21 17:31:25 +0000

    codigo ok

---
+++ Python/calculadora.py
@@ -0,0 +1,8 @@
+# Crear una calculadora con las operaciones matematicas basicas
+
+valor1 = 7 
+valor2 = 4
+
+resultado = valor1 + valor2
+
+print(resultado)
--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,22 @@
-# Crear una calculadora con las operaciones matematicas basicas
+'''
+Recibe una oración como string.
+Indica la posición de la letra con la cual empieza nuestro subtexto (la parte que vamos a “recortar”).
+Indicar la posición de la letra con la cual termina nuestro subtexto.
+Imprime el subtexto.
+'''
 
-valor1 = 7 
-valor2 = 4
-
-resultado = valor1 + valor2
+# Mi codigo
+oracion = input('Escriba una oración: ')
+posicion1 = int(input('Escriba la posición para inciar el recorte: '))
+posicion2 = int(input('Escriba la posición para finalizar: '))
+resultado = oracion[:posicion1] + oracion[posicion2:]
 
 print(resultado)
+
+#El codigo de la profe
+frase = 'las flores amarillas'
+inicial = 4
+final = 10
+palabra = frase[inicial:final]
+
+print(palabra)
