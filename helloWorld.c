a1ed00a390a4805bd8e16c9e788cd6a12ac48f4a
vhenaoi <vhenao.isaza@gmail.com>
2023-10-02 16:51:23 -0500

    completo

--- helloWorld.c
+++ helloWorld.c
@@ -10,9 +10,24 @@ int main()
 }
 /*
 Basic Types:
-    (A) Aritmeticos int 2 -32,768 to 32,767 
+    Aritmeticos
+    
+    (A) Valores enteros
+                    int 2 -32,768 to 32,767 
                     unsigned int 0 to 65,535  
+
                     int 4 -2,147,483,648 to 2,147,483,647
                     unsigned int 0 to 4,294,967,295
+
+                    short 2 Bytes -32,768 to 32,767
+                    unsigned short 2 Bytes 0 to 65,535
+
+                    long 8 Bytes -9223372036854775808 to 9223372036854775807
+                    unsigned long 0 to 18446744073709551615
+
     (B) De punto Flotante float
+
+                    float 4 Bytes 1.2E-38 to 3.4E+38   6 decimal places
+                    double 8 Bytes 2.3R-308 to 1.7E+308 15 decimal places
+                    long double 10 Bytes 3.4E-4932 to 1.1E+4932 19 decimal places
 */
