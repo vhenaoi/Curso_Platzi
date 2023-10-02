7845d5627c8e73e79e5ad288d7d1b966d6bd5382
vhenaoi <vhenao.isaza@gmail.com>
2023-10-02 16:42:15 -0500

    tipos de datos

--- helloWorld.c
+++ helloWorld.c
@@ -8,3 +8,11 @@ int main()
     printf("Hello, world!\n");
     return 0;
 }
+/*
+Basic Types:
+    (A) Aritmeticos int 2 -32,768 to 32,767 
+                    unsigned int 0 to 65,535  
+                    int 4 -2,147,483,648 to 2,147,483,647
+                    unsigned int 0 to 4,294,967,295
+    (B) De punto Flotante float
+*/
