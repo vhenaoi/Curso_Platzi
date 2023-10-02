38905c87ed47acf206e23a5e4f0569453fd09eb7
vhenaoi <vhenao.isaza@gmail.com>
2023-10-02 18:24:07 -0500

    CONSTANTES

---
+++ cons.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Variable Declarations
+const int costHotDog = 10000;
+const float ageMean = 25.3;
+const char hi = 'Hola';
+
+
+int main()
+{
+
+    printf("");
+    return 0;
+}
--- var.c
+++ var.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 // Variable Declarations
-int a, b, c;
+extern int a, b, c;
 float f, g, h; 
 
 int main()
 {
+    // Variable Declarations
+    int a, b, c;
     // Variable initialization
     a = 10;
     b = 2147483647; // Se desbordo
