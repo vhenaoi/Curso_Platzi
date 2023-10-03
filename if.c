044f191aae9152a2c413722818d509900a278997
vhenaoi <vhenao.isaza@gmail.com>
2023-10-03 11:18:36 -0500

    error

--- if.c
+++ if.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int n = 21;
+int n = -1;
 
 int main()
 {
@@ -14,10 +14,10 @@ int main()
     }
     else if (n > 20)
         printf("%d es mayor que 20", n);
-    else if (n < 10)
+    else if (n < 10 && n >= 0)
         printf("El número es menor que 10");
     else
-        printf("Error no hay npumero");  
+        printf("Error, no puede ser un número negativo");  
         printf("\n");
     return 0;
     }
