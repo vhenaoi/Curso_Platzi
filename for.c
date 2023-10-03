914bab62d7f49a2dd270598469989d62e19e1edf
vhenaoi <vhenao.isaza@gmail.com>
2023-10-03 12:20:39 -0500

    continue en for

--- for.c
+++ for.c
@@ -4,8 +4,10 @@ int n = 30;
 int i =0;
 
 int main(){
-    for(i=0; i <= n; i++){
+    for(i=0; i < n; i++){
+        continue;
         printf("El valor actual de i es %d \n", i);
     }
+    printf("El valor actual de i es %d \n", i);
     return 0;
 }
